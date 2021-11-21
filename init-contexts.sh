ed7936d5d56e34627d9cbe5ced59de3cf748dffd
Timur Batyrshin <erthad@gmail.com>
2021-11-21 16:27:45 +0300

    bugfix

--- init-contexts.sh
+++ init-contexts.sh
@@ -6,8 +6,8 @@ ssh-add .vagrant/machines/slave/virtualbox/private_key
 docker context create v0 --docker "host=ssh://vagrant@192.168.77.10"
 docker context create v1 --docker "host=ssh://vagrant@192.168.77.11"
 
-ssh-keygen -R vagrant@192.168.77.10
-ssh-keygen -R vagrant@192.168.77.11
+ssh-keygen -R 192.168.77.10
+ssh-keygen -R 192.168.77.11
 
 ssh vagrant@192.168.77.10 hostname
 ssh vagrant@192.168.77.11 hostname
