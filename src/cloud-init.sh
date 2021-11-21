500be9255ecc87d025001c629576c16b1e00bcbf
Timur Batyrshin <erthad@gmail.com>
2021-11-21 16:07:02 +0300

    use latest and greatest version of Docker and Docker Compose

--- src/cloud-init.sh
+++ src/cloud-init.sh
@@ -8,8 +8,16 @@ cat > /etc/docker/daemon.json << EOF
 }
 EOF
 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-add-repository "deb https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
+
 apt-get update
-apt-get install -y docker.io docker-compose
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+curl -L --fail https://github.com/docker/compose/releases/download/1.29.2/run.sh -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
 
 gpasswd -a vagrant docker
 
